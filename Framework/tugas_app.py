71c22bac52503960c72b4fada0ac728be985195f
Muh Wahyudi <muhwahyudi947@gmail.com>
2025-04-10 09:14:14 +0700

    revisi lembar kerja

--- Framework/tugas_app.py
+++ Framework/tugas_app.py
@@ -2,19 +2,46 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# mengarahkan page beranda
-@app.route("/beranda/")
-def beranda():
-    return "selamat datang di websit saya"
 
-# mengarhkan ke page saya(about tanpa css)
-@app.route("/beranda/about_me/saya/")
-def saya():
-    return render_template('about_1.html')
+# mengarahkan page beranda
+# @app.route("/")
+# def home():
+#     return "selamat datang di websit saya, ini adalah halaman home"
+
+
 # mengarhakan ke page about yang menggunakan css
 @app.route("/beranda/about_me/")
 def about_me():
     return render_template('about.html')
+
+# mengarahkan ke website yang bisa menambahkan string, integer dll
+@app.route("/my_name/<string:nama>")
+def getnama(nama):
+    return "hallo nama saya adalaha {}".format(nama)
+
+@app.route("/my_name2/<name>")
+def user(name):
+    return f"Hello, {name}!"
+
+@app.route('/my_name_id/<int:user_id>')  # Hanya menangani variabel selain string
+def user_id(user_id):
+    return f"User ID: {user_id}"
+
+#ke website dengan menggunakan html
+@app.route('/my_name_html/<name>')
+def my_name(name):
+    return render_template('my_name.html', username=name)
+
+# website dengan variabel global
+app_name = "My Flask App" 
+@app.route('/')
+def home():
+    return f"Welcome to {app_name}!"
+
+@app.route('/my_name_data')
+def data():
+    user = {"name": "wahyudi", "age": 19, "city": "makassar"}
+    return render_template('data.html', user=user)
 
 
 @app.route("/beranda/users/")
